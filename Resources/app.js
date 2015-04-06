8e115c7fb32269daf76d3582d52dd577efe44167
almondjoy89 <wils1977@vandals.uidaho.edu>
2015-04-06 13:21:59 -0600

    Update#3
    Sorry, more updates

--- Resources/app.js
+++ Resources/app.js
@@ -9,7 +9,7 @@ var tabGroup = Titanium.UI.createTabGroup();
 // TAB 1
 //
 var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
+    title:'VandalStore Home',
     backgroundImage:''
 });
 var tab1 = Titanium.UI.createTab({  
@@ -17,6 +17,16 @@ var tab1 = Titanium.UI.createTab({
     title:'VandalStore',
     window:win1
 });
+
+//could not get the image to upload properly but still left the code in here
+
+//win1.backgroundColor = 'white';
+//var image = Ti.UI.createImageView({
+  //image:'\VandalstoreHours.png'
+//});
+
+//win1.add(image);
+//win1.open();
 
 var label1 = Titanium.UI.createLabel({
 	color:'#999',
@@ -37,46 +47,72 @@ win1.add(label1);
 // TAB 2
 //
 var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
+    title:'Technology',
     backgroundColor:'#fff'
 });
 var tab2 = Titanium.UI.createTab({  
     icon:'KS_nav_ui.png',
-    title:'Textbooks',
+    title:'Technology',
     window:win2
 });
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'Textbooks page',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+//var label2 = Titanium.UI.createLabel({
+	//color:'#999',
+	//text:'Textbooks page',
+	//font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	//textAlign:'center',
+	//width:'auto'
+//});
+
+//This should really be a drop-down selection list instead of table, but I don't have that sort of functionality or data
+win2.backgroundColor = 'white';
+var tableData = [ {title: 'Apple', hasChild:true}, {title: 'Accessories', hasChild:true}, {title: 'Software', hasChild:true}, {title: 'Go Pro', hasChild:true} ];
+
+var table = Ti.UI.createTableView({
+  data: tableData,
+  top: 5,
+  left: 5
 });
 
-win2.add(label2);
+win2.add(table);
+win2.open();
+
+//win2.add(label2);
 
 // TAB 3
 //
 var win3 = Titanium.UI.createWindow({  
-    title:'Tab 3',
+    title:'Apparel',
     backgroundColor:'#fff'
 });
+
 var tab3 = Titanium.UI.createTab({  
     icon:'KS_nav_views.png',
-    title:'Appearl',
+    title:'Apparel',
     window:win3
 });
 
-var label3 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'Appearl page',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+//var label3 = Titanium.UI.createLabel({
+	//color:'#999',
+	//text:'Apparel page',
+	//font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	//textAlign:'center',
+	//width:'auto'
+//});
+
+win3.backgroundColor = 'white';
+var tableData = [ {title: 'All Apparel', hasChild:true}, {title: 'Kids Apparel', hasChild:true}, {title: 'Ladies Apparel', hasChild:true}, {title: 'Mens Apparel', hasChild:true} ];
+
+var table = Ti.UI.createTableView({
+  data: tableData,
+  top: 5,
+  left: 5
 });
 
-win3.add(label3);
+win3.add(table);
+win3.open();
+
+//win3.add(label3);
 
 
 //
